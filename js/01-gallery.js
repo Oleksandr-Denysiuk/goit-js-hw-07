3f5b6e3307fcd500e7029a4f7c24bce99cf5a0d5
Oleksandr-Denysiuk <saniarok4@gmail.com>
2023-04-01 19:59:02 +0200

    task completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createGallery(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGallery(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+const instance = basicLightbox.create(
+  `<img src=" " width="800" height="600">`,
+
+  {
+    onShow: () => {
+      window.addEventListener("keydown", onEscapePress, { onse: true });
+    },
+    // onClose: () => {
+    //   window.removeEventListener("keydown", onEscapePress);
+    // },
+  }
+);
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  const originPicRef = event.target.dataset.source;
+  if (!originPicRef) return;
+
+  instance.element().querySelector("img").src = originPicRef;
+  instance.show();
+}
+
+function onEscapePress(event) {
+  if (event.key !== "Escape") return;
+  instance.close();
+}
+
+galleryList.addEventListener("click", onGalleryClick);
